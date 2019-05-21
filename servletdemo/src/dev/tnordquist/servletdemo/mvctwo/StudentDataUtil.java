88cfe6cb6045f4e149a8e4594b944c43d2b90f54
Todd A Nordquist <toddnordquist144@gmail.com>
2019-05-21 13:24:42 -0600

    Added model class StudentDataUtil

---
+++ servletdemo/src/dev/tnordquist/servletdemo/mvctwo/StudentDataUtil.java
@@ -0,0 +1,20 @@
+package dev.tnordquist.servletdemo.mvctwo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDataUtil {
+	
+	List<Student> getStudents() {
+		
+		List<Student> students = new ArrayList<>();
+		
+		students.add(new Student("Mary", "Long", "mary@luv2.com"));
+		students.add(new Student("Jake", "Cornfield", "jake@luv2.com"));
+		students.add(new Student("Randal", "Schmandell", "randy@luv2.com"));
+		
+		return students;
+		
+	}
+
+}
