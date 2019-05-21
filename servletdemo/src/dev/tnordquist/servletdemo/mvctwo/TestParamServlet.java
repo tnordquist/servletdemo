4c2bff3b8423a1dcd9897f979d3bdce9becd0348
Todd A Nordquist <toddnordquist144@gmail.com>
2019-05-21 13:06:53 -0600

    Added Student service class for MVC tutorial.

---
+++ servletdemo/src/dev/tnordquist/servletdemo/mvctwo/TestParamServlet.java
@@ -0,0 +1,62 @@
+package dev.tnordquist.servletdemo.mvctwo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestParamServlet
+ */
+@WebServlet("/TestParamServlet")
+public class TestParamServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TestParamServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// set content type
+		response.setContentType("text/html");
+
+		// get printwriter
+		PrintWriter out = response.getWriter();
+
+		// read config params
+		ServletContext  context = getServletContext();
+		String maxCartSize = context.getInitParameter("max-shopping-cart-size");
+		String teamName = context.getInitParameter("team-project-name");
+		
+		out.println("<html><body>");
+		out.println("Max cart: " + maxCartSize);
+		out.println("<br/><br/>");
+		out.println("Team Name: " + teamName);
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
